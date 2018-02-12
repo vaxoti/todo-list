a48bc7971ca60a4e651e000ff10f7624460c6838
Nikita Gukov <isdelta@bk.ru>
2018-02-12 11:17:59 +0300

    crate project and add TaskList simple view

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+   
+    <TaskList :tasks="tasks" />
+    {{tasks[0].title}}
+  </div>
+</template>
+
+<script>
+import TaskList from "./components/TaskList.vue";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App1",
+      tasks: [
+        { done: true,
+          title: 'Create ToDo',
+          priority: 1,
+          date: new Date() },
+        { done: false,
+          title: 'Create ToDo222',
+          priority: 2,
+          date: new Date('12.01.2015')}
+        ]
+    };
+  },
+  components: {
+    TaskList
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>
+    List Of All Tasks
+    </h1>
+    <ul>
+      <li :key="task" v-for="task in tasks"> <input type="checkbox" v-model="task.done"> | <input type="text" v-model="task.title"> | {{task.priority}} | {{task.date.getFullYear()}}-{{task.date.getMonth()+1}}-{{task.date.getDate()}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    tasks: Array
+  }
+};
+</script>
+
+<style>
+ul {
+  margin: 0 auto;
+  text-align: left
+}
+</style>
